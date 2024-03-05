e4ea789277c423f825b4b46426f040cf018d25c9
oikos1 <albert@oikos.cash>
2024-03-05 14:37:56 -0400

    .

---
+++ src/Positions.sol
@@ -0,0 +1,25 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+
+
+
+contract Positions {
+
+    IUniswapV3Pool public pool;
+
+    function initialize() {
+    if (lock.nftId() == 0) {
+      return;
+    } else if (address(pool) == address(0)) {
+      IUniswapV3Factory factory = IUniswapV3Factory(factoryAddress);
+      pool = IUniswapV3Pool(factory.getPool(weth, address(circle), poolFee));
+      ERC20(circle).approve(address(pool), type(uint256).max);
+      WETH(payable(weth)).approve(address(pool), type(uint256).max);
+      require(address(pool) != address(0));
+      token0 = pool.token0();
+    }
+    }
+
+
+}
