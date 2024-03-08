b87ffd8db9e75200307978dddb15d019bba3eb53
oikos1 <albert@oikos.cash>
2024-03-08 12:21:57 -0400

    token contract

---
+++ src/token/AmphorToken.sol
@@ -0,0 +1,23 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+import {ERC20} from "solmate/tokens/ERC20.sol";
+import {Owned} from "solmate/auth/Owned.sol";
+
+
+contract AmphorToken is ERC20, Owned {
+    
+  constructor(address deployer, uint256 supply) 
+  ERC20("Amphor", "AMPHR", 18) 
+  Owned(msg.sender) {
+    _mint(deployer, supply);
+  }
+
+  function mintTo(address to, uint256 amount) external onlyOwner {
+    _mint(to, amount);
+  }
+
+  function renounceOwnership() external onlyOwner {
+    transferOwnership(address(0));
+  }
+}
