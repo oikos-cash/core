cc2a9c1ed4285eccd9b1a6f767731fb44652b01f
oikos1 <albert@oikos.cash>
2024-05-20 21:58:42 -0400

    mock tokens

---
+++ src/token/MockNomaToken.sol
@@ -0,0 +1,54 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.23;
+
+import "@openzeppelin/contracts-upgradeable/token/ERC20/ERC20Upgradeable.sol";
+import "@openzeppelin/contracts-upgradeable/proxy/utils/Initializable.sol";
+import "@openzeppelin/contracts-upgradeable/access/OwnableUpgradeable.sol";
+import "@openzeppelin/contracts-upgradeable/proxy/utils/UUPSUpgradeable.sol";
+import {IUniswapV3Pool} from "@uniswap/v3-core/interfaces/IUniswapV3Pool.sol";
+import {ISwapRouter} from "@uniswap/v3-periphery/interfaces/ISwapRouter.sol";
+
+contract MockNomaToken is Initializable, ERC20Upgradeable, OwnableUpgradeable, UUPSUpgradeable {
+    mapping(address => bool) public allowedPools;
+
+    function initialize(address deployer, uint256 totalSupply) initializer public {
+        __ERC20_init("Test Noma", "tNOMA");
+        _mint(deployer, totalSupply);
+        __Ownable_init(msg.sender);
+        __UUPSUpgradeable_init();
+    }
+
+    function _authorizeUpgrade(address newImplementation) internal override onlyOwner {}
+
+    function version() public pure returns (string memory) {
+        return "V1";
+    }
+
+    function proxiableUUID() public pure override returns (bytes32) {
+        return keccak256("eip1967.proxy.implementation");
+    }
+
+    function transfer(address recipient, uint256 amount) public override onlyUniswapV3(msg.sender) returns (bool) {
+        return super.transfer(recipient, amount);
+    }
+
+    function transferFrom(address sender, address recipient, uint256 amount) public override onlyUniswapV3(msg.sender) returns (bool) {
+        return super.transferFrom(sender, recipient, amount);
+    }
+
+    function addAllowedPool(address pool) external onlyOwner {
+        allowedPools[pool] = true;
+    }
+
+    function removeAllowedPool(address pool) external onlyOwner {
+        allowedPools[pool] = false;
+    }
+    
+    modifier onlyUniswapV3(address sender) {
+        require(
+            allowedPools[sender],
+            "Token can only be transferred via Uniswap V3"
+        );
+        _;
+    }
+}
