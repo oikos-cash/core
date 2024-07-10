1d6ddb5c98108d0943397d0ab20b4425594cb0ae
oikos1 <albert@oikos.cash>
2024-07-09 21:28:56 -0400

    Staking contract WIP

--- src/staking/Staking.sol
+++ src/staking/Staking.sol
@@ -1,206 +1,56 @@
 // SPDX-License-Identifier: MIT
-pragma solidity ^0.8.0;
+pragma solidity ^0.8.23;
 
-import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 import "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
-import "@openzeppelin/contracts/utils/ReentrancyGuard.sol";
-import "../libraries/SafeMath.sol";
+import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
+import "../interfaces/IsNomaToken.sol";
 
-interface IStaking {
-    function totalSupply() external view returns (uint256);
-    function balanceOf(address account) external view returns (uint256);
-    function lastTimeRewardApplicable() external view returns (uint256);
-    function rewardPerToken() external view returns (uint256);
-    function earned(address account) external view returns (uint256);
-    function getRewardForDuration() external view returns (uint256);
 
-    function stake(uint256 amount) external;
-    function withdraw(uint256 amount) external;
-    function getReward() external;
-    function exit() external;
-}
-
-interface IRewardEscrow {
-    function createEscrowEntries(address user, uint256[] calldata amounts, uint256[] calldata releaseTimes) external;
-}
-
-abstract contract RewardsDistributionRecipient is Ownable {
-    address public rewardsDistribution;
-
-    function notifyRewardAmount(uint256 reward) external virtual;
-
-    modifier onlyRewardsDistribution() {
-        require(msg.sender == rewardsDistribution, "Caller is not RewardsDistribution contract");
-        _;
-    }
-
-    function setRewardsDistribution(address _rewardsDistribution) external onlyOwner {
-        rewardsDistribution = _rewardsDistribution;
-    }
-}
-
-contract Staking is RewardsDistributionRecipient, IStaking, ReentrancyGuard {
-    using SafeMath for uint256;
+contract sStaking is Ownable {
     using SafeERC20 for IERC20;
+    using SafeERC20 for IsNomaToken;
 
-    IERC20 public nomaToken;
-    IRewardEscrow public rewardEscrow;
+    struct Epoch {
+        uint256 number; // since inception
+        uint256 end; // timestamp
+        uint256 distribute; // amount
+    }
 
-    uint256 public periodFinish = 0;
-    uint256 public rewardRate = 0;
-    uint256 public rewardsDuration = 7 days;
-    uint256 public lastUpdateTime;
-    uint256 public rewardPerTokenStored;
-    uint256 public totalStaked;
+    IERC20 public immutable NOMA;
+    IsNomaToken public immutable sNOMA;
 
-    mapping(address => uint256) public userRewardPerTokenPaid;
-    mapping(address => uint256) public rewards;
-
-    uint256 private _totalSupply;
-
-    mapping(address => uint256) private _balances;
-
-    bool public initialized;
-    bool public escrowActivated;
+    Epoch public epoch;
 
     constructor(
-        address _owner,
-        address _nomaToken
-    ) Ownable(_owner) {
-        transferOwnership(_owner);
-        nomaToken = IERC20(_nomaToken);
-        lastUpdateTime = block.timestamp;
+        address _noma,
+        address _sNoma,
+        address _authority
+    ) Ownable(_authority) {
+        require(_noma != address(0), "Zero address: NOMA");
+        NOMA = IERC20(_noma);
+        require(_sNoma != address(0), "Zero address: sNOMA");
+        sNOMA = IsNomaToken(_sNoma);
+
+        epoch = Epoch({
+            number: 1,
+            end: 0,
+            distribute: 100e18
+        });
     }
 
-    function setup(address _recipient, address _nomaToken, address _rewardEscrow) external onlyOwner {
-        require(!initialized, "Already initialized");
-        transferOwnership(_recipient);
-        nomaToken = IERC20(_nomaToken);
-        rewardEscrow = IRewardEscrow(_rewardEscrow);
-        initialized = true;
+    function stake(
+        address _to,
+        uint256 _amount
+    ) external {
+        NOMA.safeTransferFrom(msg.sender, address(this), _amount);
+        sNOMA.mint(_to, _amount);
     }
 
-    function totalSupply() external view override returns (uint256) {
-        return _totalSupply;
+    function unStake(
+        uint256 _amount
+    ) external {
+        sNOMA.safeTransferFrom(msg.sender, address(this), _amount);
+        NOMA.transfer(msg.sender, _amount);
     }
-
-    function balanceOf(address account) external view override returns (uint256) {
-        return _balances[account];
-    }
-
-    function lastTimeRewardApplicable() public view override returns (uint256) {
-        return block.timestamp < periodFinish ? block.timestamp : periodFinish;
-    }
-
-    function rewardPerToken() public view override returns (uint256) {
-        if (_totalSupply == 0) {
-            return rewardPerTokenStored;
-        }
-        uint256 _lastTimeRewardApplicable = lastTimeRewardApplicable();
-        return
-            rewardPerTokenStored.add(
-                _lastTimeRewardApplicable > lastUpdateTime ? 
-                lastTimeRewardApplicable().sub(lastUpdateTime).mul(rewardRate).mul(1e18).div(_totalSupply) :
-                lastUpdateTime.sub(_lastTimeRewardApplicable).mul(rewardRate).mul(1e18).div(_totalSupply)
-            );
-    }
-
-    function earned(address account) public view override returns (uint256) {
-        return _balances[account]
-            .mul(rewardPerToken().sub(userRewardPerTokenPaid[account]))
-            .div(1e18)
-            .add(rewards[account]);
-    }
-
-    function getRewardForDuration() external view override returns (uint256) {
-        return rewardRate.mul(rewardsDuration);
-    }
-
-    function stake(uint256 amount) external override nonReentrant updateReward(msg.sender) {
-        require(amount > 0, "Cannot stake 0");
-        _totalSupply = _totalSupply.add(amount);
-        totalStaked += amount;
-        _balances[msg.sender] = _balances[msg.sender].add(amount);
-        nomaToken.safeTransferFrom(msg.sender, address(this), amount);
-        emit Staked(msg.sender, amount);
-    }
-
-    function withdraw(uint256 amount) public override nonReentrant updateReward(msg.sender) {
-        require(amount > 0, "Cannot withdraw 0");
-        _totalSupply = _totalSupply.sub(amount);
-        totalStaked -= amount;
-        _balances[msg.sender] = _balances[msg.sender].sub(amount);
-        nomaToken.safeTransfer(msg.sender, amount);
-        emit Withdrawn(msg.sender, amount);
-    }
-
-    function getReward() public override nonReentrant updateReward(msg.sender) {
-        uint256 reward = rewards[msg.sender];
-        if (reward > 0) {
-            rewards[msg.sender] = 0;
-            nomaToken.safeTransfer(msg.sender, reward);
-            emit RewardPaid(msg.sender, reward);
-        }
-    }
-
-    function exit() external override {
-        withdraw(_balances[msg.sender]);
-        getReward();
-    }
-
-    function notifyRewardAmount(uint256 reward) external override onlyOwner updateReward(address(0)) {
-        uint256 timeSinceLastUpdate = block.timestamp - lastUpdateTime;
-        if (timeSinceLastUpdate == 0) {
-            timeSinceLastUpdate = 1; 
-        }
-        
-        if (block.timestamp >= periodFinish) {
-            rewardRate = reward.div(timeSinceLastUpdate);
-        } else {
-            uint256 remaining = periodFinish.sub(block.timestamp);
-            uint256 leftover = remaining.mul(rewardRate);
-            rewardRate = reward.add(leftover).div(timeSinceLastUpdate);
-        }
-
-        nomaToken.safeTransferFrom(msg.sender, address(this), reward);
-        uint256 balance = nomaToken.balanceOf(address(this));
-        require(rewardRate <= balance.div(timeSinceLastUpdate), "Provided reward too high");
-
-        lastUpdateTime = block.timestamp;
-        periodFinish = block.timestamp.add(rewardsDuration);
-        emit RewardAdded(reward);
-    }
-
-    function recoverERC20(address tokenAddress, uint256 tokenAmount) external onlyOwner {
-        require(tokenAddress != address(nomaToken), "Cannot withdraw the staking token");
-        IERC20(tokenAddress).safeTransfer(owner(), tokenAmount);
-        emit Recovered(tokenAddress, tokenAmount);
-    }
-
-    function setRewardsDuration(uint256 _rewardsDuration) external onlyOwner {
-        require(
-            block.timestamp > periodFinish,
-            "Previous rewards period must be complete before changing the duration for the new period"
-        );
-        rewardsDuration = _rewardsDuration;
-        emit RewardsDurationUpdated(rewardsDuration);
-    }
-
-    modifier updateReward(address account) {
-        rewardPerTokenStored = rewardPerToken();
-        lastUpdateTime = block.timestamp;
-        if (account != address(0)) {
-            rewards[account] = earned(account);
-            userRewardPerTokenPaid[account] = rewardPerTokenStored;
-        }
-        _;
-    }
-
-    event RewardAdded(uint256 reward);
-    event Staked(address indexed user, uint256 amount);
-    event Withdrawn(address indexed user, uint256 amount);
-    event RewardPaid(address indexed user, uint256 reward);
-    event RewardsDurationUpdated(uint256 newDuration);
-    event Recovered(address token, uint256 amount);
 }
