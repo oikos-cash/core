8f5c1beccdc4620a1f30144ee3fdb0c2027da92b
oikos1 <albert@oikos.cash>
2024-07-16 23:31:57 -0400

    add functions to interface

--- src/interfaces/IVault.sol
+++ src/interfaces/IVault.sol
@@ -1,9 +1,12 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.23;
 import {IUniswapV3Pool} from "@uniswap/v3-core/interfaces/IUniswapV3Pool.sol";
+import {LiquidityPosition} from "../Types.sol";
 
 interface IVault  {
     function borrow(address who, uint256 borrowAmount) external;
     function pool() external returns (IUniswapV3Pool);
     function payback(address who) external;
+    function roll(address who) external;
+    function updatePositions(LiquidityPosition[3] memory newPositions) external;
 }
