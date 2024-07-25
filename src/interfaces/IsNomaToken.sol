c085504a04830b8afa309ec24b7811e470248692
oikos1 <albert@oikos.cash>
2024-07-25 14:40:47 -0400

    edit sNoma interface

--- src/interfaces/IsNomaToken.sol
+++ src/interfaces/IsNomaToken.sol
@@ -3,7 +3,7 @@ pragma solidity ^0.8.23;
 import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 
 interface IsNomaToken is IERC20 {
-    function rebase(uint256 profit_, uint256 epoch_) external;
+    function rebase(uint256 supplyDelta) external;
     function circulatingSupply() external view returns (uint256);
     function mint(address _recipient, uint256 _amount) external;
 }
