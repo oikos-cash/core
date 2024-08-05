40b597db572fe9990788263a51a76afec7f9ba52
oikos1 <albert@oikos.cash>
2024-08-05 16:55:11 -0400

    use random rewards and stake amounts

--- test/Staking.t.sol
+++ test/Staking.t.sol
@@ -18,7 +18,9 @@ contract TestStaking is Test {
     uint256 constant INITIAL_NOMA_BALANCE = 1000e18;
     uint256 constant STAKE_AMOUNT = 100e18;
     uint256 constant INITIAL_FRAGMENTS_SUPPLY = 3_025_000 * 1e18;
-
+    uint256 constant MAX_STAKE_AMOUNT = 1000e18;
+    uint256 constant MAX_REWARD_AMOUNT = 10000e18;
+    
     function setUp() public {
         NOMA = new MockNomaToken();
         NOMA.initialize(address(this), 1000000e18); // Large initial supply
@@ -186,4 +188,131 @@ contract TestStaking is Test {
 
         assertGe(availableNOMA, circulatingSupply, "Staking contract should have enough NOMA to cover all circulating sNOMA");
     }
+
+
+    function testRandomStakingAndUnstaking() public {
+        uint256[] memory stakeAmounts = new uint256[](NUM_USERS);
+        uint256 totalStaked = 0;
+
+        // All users stake random amounts
+        for (uint i = 0; i < NUM_USERS; i++) {
+            uint256 stakeAmount = _randomAmount(1e18, MAX_STAKE_AMOUNT);
+            stakeAmounts[i] = stakeAmount;
+            totalStaked += stakeAmount;
+
+            vm.prank(users[i]);
+            staking.stake(users[i], stakeAmount);
+
+            assertEq(NOMA.balanceOf(users[i]), INITIAL_NOMA_BALANCE - stakeAmount, "NOMA balance incorrect after stake");
+            assertGt(sNOMA.balanceOf(users[i]), stakeAmount, "sNOMA balance should be slightly higher than stake amount");
+        }
+
+        // Simulate random rewards
+        uint256 totalRewards = 0;
+        for (uint i = 0; i < 5; i++) {
+            uint256 rewardAmount = _randomAmount(100e18, MAX_REWARD_AMOUNT);
+            totalRewards += rewardAmount;
+            NOMA.mint(address(staking), rewardAmount);
+            vm.prank(vault);
+            staking.notifyRewardAmount(rewardAmount);
+        }
+
+        // All users unstake
+        for (uint i = 0; i < NUM_USERS; i++) {
+            uint256 sNOMABalanceBefore = sNOMA.balanceOf(users[i]);
+            uint256 NOMABalanceBefore = NOMA.balanceOf(users[i]);
+
+            vm.prank(users[i]);
+            sNOMA.approve(address(staking), type(uint256).max);
+            staking.unstake(users[i]);
+
+            assertEq(sNOMA.balanceOf(users[i]), 0, "sNOMA balance should be 0 after unstake");
+            assertGe(NOMA.balanceOf(users[i]), NOMABalanceBefore + stakeAmounts[i], "NOMA balance should be at least the initial stake amount after unstake");
+        }
+
+        // Check if staking contract has enough NOMA to cover all unstakes
+        uint256 circulatingSupply = sNOMA.totalSupply() - sNOMA.balanceOf(address(staking));
+        uint256 stakingNOMABalance = NOMA.balanceOf(address(staking));
+        uint256 initialStakingBalance = sNOMA.balanceForGons(INITIAL_FRAGMENTS_SUPPLY);
+        uint256 availableNOMA = stakingNOMABalance > initialStakingBalance ? stakingNOMABalance - initialStakingBalance : 0;
+
+        console.log("Total staked:", totalStaked);
+        console.log("Total rewards:", totalRewards);
+        console.log("Circulating sNOMA supply:", circulatingSupply);
+        console.log("Staking contract NOMA balance:", stakingNOMABalance);
+        console.log("Initial staking balance (in current sNOMA terms):", initialStakingBalance);
+        console.log("Available NOMA for unstaking:", availableNOMA);
+
+        assertGe(availableNOMA, circulatingSupply, "Staking contract should have enough NOMA to cover all circulating sNOMA");
+    }
+
+    function testRandomStaggeredStakingAndUnstaking() public {
+        uint256[] memory stakeAmounts = new uint256[](NUM_USERS);
+        uint256 totalStaked = 0;
+
+        // Half of users stake random amounts
+        for (uint i = 0; i < NUM_USERS / 2; i++) {
+            uint256 stakeAmount = _randomAmount(1e18, MAX_STAKE_AMOUNT);
+            stakeAmounts[i] = stakeAmount;
+            totalStaked += stakeAmount;
+
+            vm.prank(users[i]);
+            staking.stake(users[i], stakeAmount);
+        }
+
+        // Simulate random rewards
+        uint256 rewardAmount = _randomAmount(100e18, MAX_REWARD_AMOUNT);
+        NOMA.mint(address(staking), rewardAmount);
+        vm.prank(vault);
+        staking.notifyRewardAmount(rewardAmount);
+
+        // Other half of users stake random amounts
+        for (uint i = NUM_USERS / 2; i < NUM_USERS; i++) {
+            uint256 stakeAmount = _randomAmount(1e18, MAX_STAKE_AMOUNT);
+            stakeAmounts[i] = stakeAmount;
+            totalStaked += stakeAmount;
+
+            vm.prank(users[i]);
+            staking.stake(users[i], stakeAmount);
+        }
+
+        // Simulate more random rewards
+        uint256 additionalReward = _randomAmount(100e18, MAX_REWARD_AMOUNT);
+        rewardAmount += additionalReward;
+        NOMA.mint(address(staking), additionalReward);
+        vm.prank(vault);
+        staking.notifyRewardAmount(additionalReward);
+
+        // All users unstake
+        for (uint i = 0; i < NUM_USERS; i++) {
+            uint256 sNOMABalanceBefore = sNOMA.balanceOf(users[i]);
+            uint256 NOMABalanceBefore = NOMA.balanceOf(users[i]);
+
+            vm.prank(users[i]);
+            sNOMA.approve(address(staking), type(uint256).max);
+            staking.unstake(users[i]);
+
+            assertEq(sNOMA.balanceOf(users[i]), 0, "sNOMA balance should be 0 after unstake");
+            assertGe(NOMA.balanceOf(users[i]), NOMABalanceBefore + stakeAmounts[i], "NOMA balance should be at least the initial stake amount after unstake");
+        }
+
+        // Check if staking contract has enough NOMA to cover all unstakes
+        uint256 circulatingSupply = sNOMA.totalSupply() - sNOMA.balanceOf(address(staking));
+        uint256 stakingNOMABalance = NOMA.balanceOf(address(staking));
+        uint256 initialStakingBalance = sNOMA.balanceForGons(INITIAL_FRAGMENTS_SUPPLY);
+        uint256 availableNOMA = stakingNOMABalance > initialStakingBalance ? stakingNOMABalance - initialStakingBalance : 0;
+
+        console.log("Total staked:", totalStaked);
+        console.log("Total rewards:", rewardAmount);
+        console.log("Circulating sNOMA supply:", circulatingSupply);
+        console.log("Staking contract NOMA balance:", stakingNOMABalance);
+        console.log("Initial staking balance (in current sNOMA terms):", initialStakingBalance);
+        console.log("Available NOMA for unstaking:", availableNOMA);
+
+        assertGe(availableNOMA, circulatingSupply, "Staking contract should have enough NOMA to cover all circulating sNOMA");
+    }
+
+    function _randomAmount(uint256 min, uint256 max) internal view returns (uint256) {
+        return min + (uint256(keccak256(abi.encodePacked(block.timestamp, block.prevrandao))) % (max - min + 1));
+    }    
 }
