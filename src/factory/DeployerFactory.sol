d3f524f75aaead64d4eae6c5d2c4a3a3bf9b5a39
oikos1 <albert@oikos.cash>
2024-12-20 21:40:14 -0400

    add func. parameter

--- src/factory/DeployerFactory.sol
+++ src/factory/DeployerFactory.sol
@@ -9,11 +9,11 @@ contract DeployerFactory {
 
     /**
      * @notice Deploys a new instance of the Deployer contract
-     * @param owner The owner address for the Deployer contract
+     * @param _owner The owner address for the Deployer contract
      * @return The address of the newly deployed Deployer contract
      */
-    function deployDeployer(address owner) public returns (address) {
-        Deployer deployer = new Deployer(owner);
+    function deployDeployer(address _owner, address _resolver) public returns (address) {
+        Deployer deployer = new Deployer(_owner, _resolver);
         address deployerAddress = address(deployer);
 
         emit DeployerCreated(deployerAddress);
