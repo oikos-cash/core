597e87661969dda1c1ccd0bc2bd2d57889e4bc5c
oikos1 <albert@oikos.cash>
2025-01-13 12:00:00 -0400

    add function definition

--- src/interfaces/IModelHelper.sol
+++ src/interfaces/IModelHelper.sol
@@ -18,4 +18,5 @@ interface IModelHelper{
     function getExcessReserveBalance(address pool, address vault, bool isToken0) external view returns (uint256);
     function getIntrinsicMinimumValue(address _vault) external view returns (uint256);
     function enforceSolvencyInvariant(address _vault) external view;
+    function getTotalSupply(address pool, bool isToken0) external view returns (uint256);
 }
