6588f1154ddc410c2c7ac640b6cec4c53997efde
oikos1 <albert@oikos.cash>
2025-01-29 00:32:01 -0400

    do not set on init addresses in resolver, move code to/from facets etc

--- src/vault/BaseVault.sol
+++ src/vault/BaseVault.sol
@@ -19,6 +19,7 @@ import "../libraries/DecimalMath.sol";
 import "../libraries/Utils.sol";
 
 import {IERC20} from "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+
 interface INomaFactory {
     function mintTokens(address to, uint256 amount) external;
     function burnFor(address from, uint256 amount) external;
@@ -27,6 +28,10 @@ interface INomaFactory {
 
 interface IAdaptiveSupplyController {
     function adjustSupply(address pool, address vault, int256 volatility) external returns (uint256, uint256);
+}
+
+interface ILendingVault {
+    function setFee(uint256 fee, uint256 feeToTreasury) external;
 }
 
 error AlreadyInitialized();
@@ -72,10 +77,8 @@ contract BaseVault is OwnableUninitialized {
         address _owner,
         address _deployer,
         address _pool, 
-        address _modelHelper,
         address _stakingContract,
         address _proxyAddress,
-        address _adaptiveSupplyController,
         LiquidityStructureParameters memory _params
     ) public onlyFactory {
         LibDiamond.DiamondStorage storage ds = LibDiamond.diamondStorage();
@@ -87,14 +90,12 @@ contract BaseVault is OwnableUninitialized {
         _v.resolver = IAddressResolver(ds.resolver);
         _v.pool = IUniswapV3Pool(_pool);
         _v.factory = _factory;
-        _v.modelHelper = _modelHelper;
         _v.tokenInfo.token0 = _v.pool.token0();
         _v.tokenInfo.token1 = _v.pool.token1();
         _v.initialized = false;
         _v.stakingEnabled = false;
         _v.stakingContract = _stakingContract;
         _v.proxyAddress = _proxyAddress;
-        _v.adaptiveSupplyController = _adaptiveSupplyController;
         _v.deployerContract = _deployer;
         _v.liquidityStructureParameters = _params;
         
@@ -120,45 +121,28 @@ contract BaseVault is OwnableUninitialized {
         _v.discoveryPosition = positions[2];
     }
 
+    function setFees(
+        uint256 _feesAccumulatedToken0, 
+        uint256 _feesAccumulatedToken1
+    ) public  onlyInternalCalls {
+        _v.feesAccumulatorToken0 += _feesAccumulatedToken0;
+        _v.feesAccumulatorToken1 += _feesAccumulatedToken1;
+    }
+
     function getUnderlyingBalances(
         LiquidityType liquidityType
     ) external view 
     returns (int24, int24, uint256, uint256) {
 
-        return IModelHelper(_v.modelHelper)
+        return IModelHelper(
+            modelHelper()
+        )
         .getUnderlyingBalances(
             address(_v.pool), 
             address(this), 
             liquidityType
         ); 
-    }
-
-    function mintTokens(
-        address to,
-        uint256 amount
-    ) public onlyInternalCalls {
-        
-        _v.timeLastMinted = block.timestamp;
-
-        INomaFactory(_v.factory)
-        .mintTokens(
-            to,
-            amount
-        );
-    }
-
-    function burnTokens(
-        uint256 amount
-    ) public onlyInternalCalls {
-
-        IERC20(_v.pool.token0()).approve(address(_v.factory), amount);
-        INomaFactory(_v.factory)
-        .burnFor(
-            address(this),
-            amount
-        );
-    }
-    
+    }    
 
     function getVaultInfo() public view 
     returns (
@@ -167,7 +151,9 @@ contract BaseVault is OwnableUninitialized {
         (
             vaultInfo
         ) =
-        IModelHelper(_v.modelHelper)
+        IModelHelper(
+            modelHelper()
+        )
         .getVaultInfo(
             address(_v.pool), 
             address(this), 
@@ -175,21 +161,11 @@ contract BaseVault is OwnableUninitialized {
         );
     }
 
-    function getCollateralAmount() public view returns (uint256) {
-        return _v.collateralAmount;
-    }
-
-    function pool() public view returns (IUniswapV3Pool) {
-        return _v.pool;
-    }
-
-    function getAccumulatedFees() public view returns (uint256, uint256) {
-        return (_v.feesAccumulatorToken0, _v.feesAccumulatorToken1);
-    }
-
     function getExcessReserveToken1() external view returns (uint256) {
         bool isToken0 = false;
-        return IModelHelper(_v.modelHelper)
+        return IModelHelper(
+            modelHelper()
+        )
         .getExcessReserveBalance(
             address(_v.pool), 
             address(this), 
@@ -202,9 +178,50 @@ contract BaseVault is OwnableUninitialized {
             pool: address(_v.pool),
             vault: address(this),
             deployer: _v.deployerContract,
-            modelHelper: _v.modelHelper,
-            adaptiveSupplyController: _v.adaptiveSupplyController
+            modelHelper: modelHelper(),
+            adaptiveSupplyController: adaptiveSupply()
         });
+    }
+
+    // *** ADDRESS RESOLVER *** //
+
+    function _getResolver() internal view returns (IAddressResolver resolver) {
+        resolver = _v.resolver;
+        if (address(resolver) == address(0)) {
+            // Fallback to Diamond storage
+            LibDiamond.DiamondStorage storage ds = LibDiamond.diamondStorage();
+            resolver = IAddressResolver(ds.resolver);
+        }
+    }
+
+    function adaptiveSupply() public view returns (address) {
+        IAddressResolver resolver = _getResolver();
+        return resolver
+            .requireAndGetAddress(
+                Utils.stringToBytes32("AdaptiveSupply"), 
+                "no AdaptiveSupply"
+            );
+    }
+
+    function modelHelper() public view returns (address) {
+        IAddressResolver resolver = _getResolver();
+        return resolver
+            .requireAndGetAddress(
+                Utils.stringToBytes32("ModelHelper"), 
+                "no ModelHelper"
+            );
+    }
+
+    // *** MODIFIERS *** //
+
+    modifier onlyDeployer() {
+        if (msg.sender != _v.deployerContract) revert OnlyDeployer();
+        _;
+    }
+
+    modifier onlyInternalCalls() {
+        if (msg.sender != _v.factory && msg.sender != address(this)) revert OnlyInternalCalls();
+        _;        
     }
 
     modifier onlyFactory() {
@@ -218,45 +235,17 @@ contract BaseVault is OwnableUninitialized {
         _;
     }
 
-    function _getResolver() internal view returns (IAddressResolver resolver) {
-        resolver = _v.resolver;
-        if (address(resolver) == address(0)) {
-            // Fallback to Diamond storage
-            LibDiamond.DiamondStorage storage ds = LibDiamond.diamondStorage();
-            resolver = IAddressResolver(ds.resolver);
-        }
-    }
-
-    function getTimeSinceLastMint() public view returns (uint256) {
-        return block.timestamp - _v.timeLastMinted;
-    }
-
-
-    modifier onlyDeployer() {
-        if (msg.sender != _v.deployerContract) revert OnlyDeployer();
-        _;
-    }
-
-    modifier onlyInternalCalls() {
-        if (msg.sender != _v.factory && msg.sender != address(this)) revert OnlyInternalCalls();
-        _;        
-    }
-
     function getFunctionSelectors() external pure virtual returns (bytes4[] memory) {
-        bytes4[] memory selectors = new bytes4[](13);
+        bytes4[] memory selectors = new bytes4[](9);
         selectors[0] = bytes4(keccak256(bytes("getVaultInfo()")));
-        selectors[1] = bytes4(keccak256(bytes("pool()")));
-        selectors[2] = bytes4(keccak256(bytes("initialize(address,address,address,address,address,address,address,address,(uint8,uint8,uint8,uint16[2],uint256,uint256,int24,int24,int24,uint256,uint256,uint256))")));
-        selectors[3] = bytes4(keccak256(bytes("initializeLiquidity((int24,int24,uint128,uint256)[3])")));
-        selectors[4] = bytes4(keccak256(bytes("uniswapV3MintCallback(uint256,uint256,bytes)")));
-        selectors[5] = bytes4(keccak256(bytes("getUnderlyingBalances(uint8)")));
-        selectors[6] = bytes4(keccak256(bytes("getAccumulatedFees()")));
-        selectors[7] = bytes4(keccak256(bytes("getExcessReserveToken1()")));
-        selectors[8] = bytes4(keccak256(bytes("getCollateralAmount()")));
-        selectors[9] = bytes4(keccak256(bytes("getProtocolAddresses()")));
-        selectors[10] = bytes4(keccak256(bytes("mintTokens(address,uint256)")));
-        selectors[11] = bytes4(keccak256(bytes("getTimeSinceLastMint()")));
-        selectors[12] = bytes4(keccak256(bytes("burnTokens(uint256)")));
+        selectors[1] = bytes4(keccak256(bytes("initialize(address,address,address,address,address,address,(uint8,uint8,uint8,uint16[2],uint256,uint256,int24,int24,int24,uint256,uint256,uint256))")));
+        selectors[2] = bytes4(keccak256(bytes("initializeLiquidity((int24,int24,uint128,uint256,int24)[3])")));
+        selectors[3] = bytes4(keccak256(bytes("uniswapV3MintCallback(uint256,uint256,bytes)")));
+        selectors[4] = bytes4(keccak256(bytes("getUnderlyingBalances(uint8)")));
+        selectors[5] = bytes4(keccak256(bytes("getAccumulatedFees()")));
+        selectors[6] = bytes4(keccak256(bytes("getExcessReserveToken1()")));
+        selectors[7] = bytes4(keccak256(bytes("getProtocolAddresses()")));
+        selectors[8] = bytes4(keccak256(bytes("setFees(uint256,uint256)")));
         return selectors;
     }
 
