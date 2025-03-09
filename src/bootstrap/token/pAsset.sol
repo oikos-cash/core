bec97daf8aa928a62d78e7db33d9c52a6afb7f8a
oikos1 <albert@oikos.cash>
2025-03-09 18:55:40 -0400

    remove comments

--- src/bootstrap/token/pAsset.sol
+++ src/bootstrap/token/pAsset.sol
@@ -4,8 +4,7 @@ pragma solidity ^0.8.20;
 import { ERC20 } from "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 
 /// @title pAsset Token
-/// @notice An ERC20 token implementation based on Solmate's minimal ERC20.
-/// @dev This contract extends Solmate's ERC20, allowing custom name, symbol, and decimals at deployment.
+/// @notice An ERC20 token implementation.
 contract pAsset is ERC20 {
 
     /// @notice Creates a new pAsset token.
