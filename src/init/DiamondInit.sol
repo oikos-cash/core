b25e00510e6915c7f872d25189a3aaa2d8b0bc38
oikos1 <albert@oikos.cash>
2025-03-09 14:23:19 -0400

    disallow contract reinitialization

--- src/init/DiamondInit.sol
+++ src/init/DiamondInit.sol
@@ -14,25 +14,43 @@ import {IDiamondLoupe} from "../interfaces/IDiamondLoupe.sol";
 import {IDiamondCut} from "../interfaces/IDiamondCut.sol";
 import {IERC173} from "../interfaces/IERC173.sol";
 import {IERC165} from "../interfaces/IERC165.sol";
+import { Utils } from "../libraries/Utils.sol";
+import { IAddressResolver } from "../interfaces/IAddressResolver.sol";
 
 error AlreadyInitialized();
 error InvalidResolver();
+error NotAuthorized();
 
 contract DiamondInit {
 
-    function init(address _resolver) external {
+    address public owner;
+    address public factory;
+    bool public initialized;
+
+    function init(address _resolver) external notInitialized {
+        if (_resolver == address(0)) {
+            revert InvalidResolver();
+        }
+
         // adding ERC165 data
         LibDiamond.DiamondStorage storage ds = LibDiamond.diamondStorage();
         ds.supportedInterfaces[type(IERC165).interfaceId] = true;
         ds.supportedInterfaces[type(IDiamondCut).interfaceId] = true;
         ds.supportedInterfaces[type(IDiamondLoupe).interfaceId] = true;
         ds.supportedInterfaces[type(IERC173).interfaceId] = true;
-        
-        if (_resolver == address(0)) {
-            revert InvalidResolver();
-        }
 
+        // Set resolver
         ds.resolver = _resolver;
+
+        // Initialize flag
+        initialized = true;
+    }
+    
+    modifier notInitialized() {
+        if (initialized == true) {
+            revert AlreadyInitialized();
+        }
+        _;
     }
 
     function getFunctionSelectors() external pure virtual returns (bytes4[] memory) {
