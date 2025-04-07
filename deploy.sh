21031b8f680238126a61f8b4921eb0026d282824
oikos1 <albert@oikos.cash>
2025-04-07 19:20:28 -0400

    add optimize flag to deploy script

--- deploy.sh
+++ deploy.sh
@@ -16,8 +16,8 @@ DEPLOY_LOG=$(mktemp)
 RAW_TMP="$(dirname "$0")/deploy_helper/raw.tmp"
 
 # Step 1: Capture output of the deployment commands
-forge script script/deploy/DeployFactory.s.sol -vv --rpc-url="$RPC_URL" --private-key="$PRIVATE_KEY" --gas-price 1000000000 --broadcast --slow --legacy | tee "$DEPLOY_LOG"
-forge script script/deploy/DeployVault.s.sol -vv --rpc-url="$RPC_URL" --private-key="$PRIVATE_KEY" --gas-price 1000000000 --broadcast --slow --legacy | tee -a "$DEPLOY_LOG"
+forge script script/deploy/DeployFactory.s.sol -vv --rpc-url="$RPC_URL" --private-key="$PRIVATE_KEY" --gas-price 1000000000 --broadcast --slow --legacy --optimize | tee "$DEPLOY_LOG"
+forge script script/deploy/DeployVault.s.sol -vv --rpc-url="$RPC_URL" --private-key="$PRIVATE_KEY" --gas-price 1000000000 --broadcast --slow --legacy --optimize | tee -a "$DEPLOY_LOG"
 
 # Step 2: Extract relevant logs into a temporary file
 awk '
